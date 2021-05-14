450c8efcfb43524ea5816fc8f74dd4d3b0c0a221
soc4585 <soc4585@gmail.com>
2021-05-14 18:18:46 +0900

    주석 및 약간의 수정

--- project-ten/src/components/MyListForm.vue
+++ project-ten/src/components/MyListForm.vue
@@ -21,10 +21,13 @@ export default {
   methods: {
     // helper를 통해서 actions를 사용할 때
     // this.inputText를 어떻게 갱신하지?
-
+    // helper를 언제 쓰느냐 ? -> store에 있는 것만 조종할 때
+    // 만약 현재 위치에서 실행해야 할 동작이 있으면
+    // dispatch를 통해 actions 메서드를 호출하고, 여기서 할 일은 별도로 진행
     // ...mapActions(['createMovie',]),
+
     createMovie: function () {
-      this.$store.dispatch('createMovie', this.inputText)
+      this.createMovie(this.inputText)
       this.inputText = ''
     }
   }
